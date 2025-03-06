a6455bb61b9ac988bcc7d10757a7bb32c53d93a8
yer4zz <work.yer4ss@gmail.com>
2025-03-07 00:26:07 +0500

    Add files via upload

---
+++ task 3/script.js
@@ -0,0 +1,9 @@
+let don = document.getElementsByClassName('div1');
+don.addEventListener('click', function(){
+   console.log('clicked');
+});
+
+let erondon =  document.getElementsByTagName('p')
+erondon.addEventListener('mousover', function(){
+   console.log('hovered');
+});
